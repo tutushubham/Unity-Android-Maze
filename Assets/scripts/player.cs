7f59a8bc2084e580b8688b1b720926b7ab7e1522
tutushubham <tutushubham@gmail.com>
2019-08-26 17:33:37 +0530

    basic working

---
+++ Assets/scripts/player.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+	private bool tap, swipeleft, swiperight, swipeup, swipedown , isDraging;
+	private Vector2 startTouch, swipeDelta;
+
+	public Vector2 SwipeDelta{ get { return swipeDelta; }}
+	public bool SwipeLeft  { get { return swipeleft; }}
+	public bool SwipeRight  { get { return swiperight; }}
+	public bool SwipeUp  { get { return swipeup; }}
+	public bool SwipeDown  { get { return swipedown; }}
+
+	private void Update(){
+		tap = swipeleft = swiperight = swipeup = swipedown = false;
+
+		if (Input.GetMouseButtonDown (0)) {
+			tap = true;
+			isDraging = true;
+			startTouch = Input.mousePosition;
+		}
+		else if (Input.GetMouseButtonUp (0)) {
+			isDraging = false;
+			Reset ();
+		}
+
+		if(Input.touches.Length > 0){
+			if(Input.touches[0].phase ==TouchPhase.Began ){
+				tap = true;
+				isDraging = true;
+				startTouch = Input.touches[0].position;
+			}
+			else if(Input.touches[0].phase ==TouchPhase.Ended || Input.touches[0].phase ==TouchPhase.Canceled){
+				isDraging = false;
+				Reset();
+			}
+		}
+
+		swipeDelta = Vector2.zero;
+		if(isDraging){
+			if(Input.touches.Length > 0){
+				swipeDelta = Input.touches[0].position - startTouch;
+				swipeDelta = swipeDelta *10;
+			}
+			else if(Input.GetMouseButton(0)){
+				swipeDelta = (Vector2)Input.mousePosition - startTouch;
+				swipeDelta = swipeDelta *2;
+			}
+
+		}
+
+		if(swipeDelta.magnitude > 100){
+
+			float x =swipeDelta.x;
+			float y =swipeDelta.y;
+
+			if(Mathf.Abs(x) > Mathf.Abs(y)){
+				if(x < 0){
+					swipeleft = true;
+				}
+				else{
+					swiperight = true;
+				}
+			}
+			else{
+				if(y < 0){
+					swipedown = true;
+				}
+				else{
+					swipeup = true;
+				}
+			}
+
+			Reset();
+		}
+
+
+	}
+	private void Reset(){
+		startTouch = swipeDelta = Vector2.zero;
+		isDraging = false;
+	}
+
+}
---
+++ Assets/scripts/swipeTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class swipeTest : MonoBehaviour {
+
+	public player swipeControls;
+    public Transform Player;
+    private Vector3 desiredPosition ;
+
+	private void Start() {
+        desiredPosition = Player.position;
+    }
+
+    private void Update() {
+
+
+        if (swipeControls.SwipeLeft)
+            {desiredPosition += 2.0f * Vector3.left;}
+        if (swipeControls.SwipeRight)
+            {desiredPosition += 2.0f * Vector3.right;}
+		if (swipeControls.SwipeUp)
+            {desiredPosition += 2.0f * Vector3.forward;}
+		if (swipeControls.SwipeDown)
+            {desiredPosition += 2.0f * Vector3.back;}
+
+        Player.transform.position = Vector3.MoveTowards
+            (Player.transform.position, desiredPosition,  5.0f*Time.deltaTime);
+
+    }
+}
